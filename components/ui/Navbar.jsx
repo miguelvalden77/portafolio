1395cb13fdc3a2eb8313e1cc65bf0173d39eb3fe
miguelvalden <miguelvalden77@gmail.com>
2023-07-17 04:03:00 +0200

    header styles

--- components/ui/Navbar.jsx
+++ components/ui/Navbar.jsx
@@ -1,14 +1,15 @@
 import Link from "next/link"
 import logo from "../../public/logo.svg"
 import Image from "next/image"
+import styles from "../../styles/Navbar.module.css"
 
 
 export const Navbar = () =>{
 
     return(
-        <header>
-            <Image src={logo} alt="foto" width={100} height={100}/>
-            <nav>
+        <header className={styles.header}>
+            <Image src={logo} alt="foto" width={80} height={80}/>
+            <nav className={styles.nav}>
                <Link href="#">Home</Link> 
                <Link href="#">Proyectos</Link> 
                <Link href="#">Sobre mí</Link> 
